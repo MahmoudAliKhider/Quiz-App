419cd5d29b409ab8197917d88c5ff9ef50e7115b
MahmoudAliKhider <mahmoudkhider37@gmail.co>
2023-12-24 19:12:25 +0200

    first commit

---
+++ client/src/components/App.js
@@ -0,0 +1,27 @@
+import '../styles/App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Main from "../components/Main";
+import Quiz from "../components/Quiz";
+import Result from "../components/Result"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>
+  },
+  {
+    path: "/quiz",
+    element: <Quiz></Quiz>
+  },
+  {
+    path: "/result",
+    element: <Result></Result>
+  }
+])
+function App() {
+  return (
+    <RouterProvider router={router} />
+  );
+}
+
+export default App;
